546e70bdc2153a145f67bd197638de3debf79d5d
hesida <Romens@hsd#123>
2023-05-22 07:00:21 +0800

    add exercise2.3 file

---
+++ The Go Programming Language/2.ProgramStructure/exercise2.3.go
@@ -0,0 +1,1 @@
+package mainvar pc [256]bytefunc init() {	for i := range pc {		pc[i] = pc[i/2] + byte(i&1)	}}func PopCount(x uint64) int {	var res int	for i := 0; i < 8; i++ {		res += int(pc[byte(x>>(i*8))])	}	return res}
