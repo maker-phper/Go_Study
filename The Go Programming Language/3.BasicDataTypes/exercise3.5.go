73d8f674464ce175a89917efa3f8f6e8669469f2
Sloan <sloanhe@techtrex.com>
2023-06-09 14:06:18 +0800

    add exercise3.5 file

---
+++ The Go Programming Language/3.BasicDataTypes/exercise3.5.go
@@ -0,0 +1,61 @@
+// Mandelbrot emits a PNG image of the Mandelbrot fractal
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/png"
+	"math"
+	"math/cmplx"
+	"os"
+)
+
+func main() {
+	const (
+		xmin, ymin, xmax, ymax = -2, -2, +2, +2
+		width, height          = 1024, 1024
+	)
+
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	for py := 0; py < height; py++ {
+		y := float64(py)/height*(ymax-ymin) + ymin
+		for px := 0; px < width; px++ {
+			x := float64(px)/width*(xmax-xmin) + xmin
+			z := complex(x, y)
+			// Image point (px, py) represents complex value z.
+			img.Set(px, py, mandelbrot(z))
+		}
+	}
+
+	f1, err := os.Create("mandelbrot.png") //file in build config Working directory
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f1.Close()
+
+	//png.Encode(os.Stdout, img) // NOTE: ignoring errors
+	png.Encode(f1, img) // NOTE: ignoring errors
+}
+
+func mandelbrot(z complex128) color.Color {
+	const iterations = 200
+	const contrast = 15
+
+	var v complex128
+	for n := uint8(0); n < iterations; n++ {
+		v = v*v + z
+		if cmplx.Abs(v) > 2 {
+			switch {
+			case n > 50:
+				return color.RGBA{100, 0, 0, 255}
+			default:
+				// logarithmic blue gradient to show small differences on the
+				// periphery of the fractal.
+				logScale := math.Log(float64(n)) / math.Log(float64(iterations))
+				return color.RGBA{0, 0, 255 - uint8(logScale*255), 255}
+			}
+		}
+	}
+	return color.Black
+}
