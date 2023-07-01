7a1e99e9ba533fd9e6897d758eb0a5d660575519
hesida <hesida@romens.cn>
2023-07-02 07:06:31 +0800

    modify append example

--- The Go Programming Language/4.CompositeTypes/append.go
+++ The Go Programming Language/4.CompositeTypes/append.go
@@ -1,5 +1,16 @@
 package main
 
+import "fmt"
+
+func main() {
+	var x, y []int
+	for i := 0; i < 10; i++ {
+		y = appendInt(x, i)
+		fmt.Printf("%d  cap=%d \t%v\n", i, cap(y), y)
+		x = y
+	}
+}
+
 func appendInt(x []int, y int) []int {
 	var z []int
 	zlen := len(x) + 1
