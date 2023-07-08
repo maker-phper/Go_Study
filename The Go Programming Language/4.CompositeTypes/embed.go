c79c36dae4693ce6849898581409dcd79fc6d71e
hesida <hesida@romens.cn>
2023-07-08 09:49:17 +0800

    add embed example

---
+++ The Go Programming Language/4.CompositeTypes/embed.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+type Point struct {
+	X, Y int
+}
+
+type Circle struct {
+	Point
+	Radius int
+}
+
+type Wheel struct {
+	Circle
+	Spokes int
+}
+
+func main() {
+	w := Wheel{Circle{Point{8, 8}, 5}, 20}
+	fmt.Printf("%#v\n", w)
+
+	w = Wheel{Circle: Circle{Point: Point{X: 8, Y: 9}, Radius: 5}, Spokes: 20}
+	fmt.Printf("%#v\n", w)
+}
