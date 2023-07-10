5fc29bf85aef1db36eb4a21f5b46e3c5a04a4799
hesida <Romens@hsd#123>
2023-07-10 23:35:58 +0800

    improve movie example

--- The Go Programming Language/4.CompositeTypes/movie.go
+++ The Go Programming Language/4.CompositeTypes/movie.go
@@ -35,4 +35,10 @@ func main() {
 		log.Fatalf("Json marshaling failed: %s", err)
 	}
 	fmt.Printf("%s\n", data)
+
+	var titles []struct{ Title string }
+	if err := json.Unmarshal(data, &titles); err != nil {
+		log.Fatalf("JSON numarshling failed: %s", err)
+	}
+	fmt.Println(titles) // "[{Casablanca} {Cool Hand Luke} {Bullitt}]
 }
