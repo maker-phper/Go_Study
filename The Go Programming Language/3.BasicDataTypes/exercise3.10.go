5933b5380b0437745a356187759e89bd02cfb515
Sloan <sloanhe@techtrex.com>
2023-07-18 17:29:02 +0800

    fix format bug

--- The Go Programming Language/3.BasicDataTypes/exercise3.10.go
+++ The Go Programming Language/3.BasicDataTypes/exercise3.10.go
@@ -7,16 +7,26 @@ import (
 
 func commaUseBuffer(s string) string {
 	var buf bytes.Buffer
-	for i, v := range s {
+	sNew := reversionStr(s)
+	for i, v := range sNew {
 		if i > 0 && i%3 == 0 {
 			buf.WriteString(",")
 		}
 		fmt.Fprintf(&buf, "%s", string(v))
 
 	}
-	return buf.String()
+	return reversionStr(buf.String())
+}
+
+func reversionStr(s string) string {
+	sLen := len(s)
+	sNew := make([]byte, sLen, sLen)
+	for i, n := range s {
+		sNew[sLen-i-1] = byte(n)
+	}
+	return string(sNew)
 }
 
 func main() {
-	fmt.Println(commaUseBuffer("123456789"))
+	fmt.Println(commaUseBuffer("1234567891"))
 }
