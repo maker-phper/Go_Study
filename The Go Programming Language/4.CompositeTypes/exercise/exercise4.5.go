74a0b6e9885f8a5405c65123133d1657187dddf5
Sloan <sloanhe@techtrex.com>
2023-08-15 16:29:03 +0800

    add exexcise4.5 file

---
+++ The Go Programming Language/4.CompositeTypes/exercise/exercise4.5.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := []string{"a", "a", "b", "b", "b", "c", "d", "d", "e"}
+	res := eliminateDuplicatesElement(s)
+	res_u := unique(s)
+	fmt.Println(res)
+	fmt.Println(res_u)
+}
+func eliminateDuplicatesElement(str []string) []string {
+	last := str[len(str)-1]
+	res := make([]string, len(str))
+	for i := 0; i < len(str); i++ {
+		ss := str[i]
+		if i+1 < len(str) && ss == str[i+1] {
+			copy(str[i:], str[i+1:])
+			if last == str[i] {
+				res = str[:i+1]
+				break
+			}
+			i--
+		}
+	}
+	return res
+}
+
+//https://github.com/torbiak/gopl/blob/master/ex4.5/unique.go
+func unique(strs []string) []string {
+	w := 0
+	for _, s := range strs {
+		if strs[w] == s {
+			continue
+		}
+		w++
+		strs[w] = s
+	}
+	return strs[:w+1]
+}
