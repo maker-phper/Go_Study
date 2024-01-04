cff41fc611c87c758d2ed99b23cf52d439d7d59b
Sloan <sloanhe@techtrex.com>
2024-01-04 17:05:41 +0800

    fix linklist stack bug and resize arr stack

--- algorithm_Princeton/DataStructure/Stack.go
+++ algorithm_Princeton/DataStructure/Stack.go
@@ -16,6 +16,13 @@ type ArrStack struct {
 	n     int
 }
 
+func NewArrStack() *ArrStack {
+	return &ArrStack{
+		items: make([]string, 2),
+		n:     -1,
+	}
+}
+
 func (s *Stack) Push(item string) {
 	newNode := &Node{item: item, next: s.top}
 	s.top = newNode
@@ -31,7 +38,7 @@ func (s *Stack) Pop() string {
 }
 
 func (s *Stack) IsEmpty() bool {
-	if s.top.item == "" {
+	if s.top == nil {
 		return true
 	}
 	return false
@@ -46,24 +53,48 @@ func (as *ArrStack) IsEmpty() bool {
 
 func (as *ArrStack) Pop() string {
 	item := as.items[as.n]
+	as.items[as.n] = ""
 	as.n--
+	if as.n+1 == len(as.items)/4 {
+		as.Resize(len(as.items) / 2)
+	}
 	return item
 }
 
 func (as *ArrStack) Push(item string) {
+	if as.n+1 == len(as.items) {
+		as.Resize(2 * len(as.items))
+	}
 	as.n = as.n + 1
 	as.items[as.n] = item
+
+}
+
+func (as *ArrStack) Resize(size int) {
+	newItems := make([]string, size)
+	copy(newItems, as.items)
+	as.items = newItems
 }
 
 func main() {
 	s := Stack{}
 	s.Push("1")
+	s.Push("2")
+	s.Push("3")
+	fmt.Println(s.Pop())
+	fmt.Println(s.Pop())
 	fmt.Println(s.Pop())
 	fmt.Println(s.IsEmpty())
 
-	size := 10
-	as := &ArrStack{items: make([]string, size)}
+	as := NewArrStack()
 	as.Push("a1")
+	as.Push("a2")
+	as.Push("a3")
+	as.Push("a4")
+	fmt.Println(as)
 	fmt.Println(as.Pop())
+	fmt.Println(as.Pop())
+	fmt.Println(as.Pop())
+	fmt.Println(len(as.items))
 	fmt.Println(as.IsEmpty())
 }
