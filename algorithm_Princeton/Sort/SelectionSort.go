9516245c200346b3cc693d860bf17bdbbcacf082
Sloan <sloanhe@techtrex.com>
2024-03-08 17:28:43 +0800

    add selection sort and insertion sort file

---
+++ algorithm_Princeton/Sort/SelectionSort.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{5, 4, 6, 3, 1, 2, 7}
+	fmt.Println(SelectionSort(arr))
+}
+
+func SelectionSort(arr []int) []int {
+	for i := 1; i < len(arr); i++ {
+		for j := i; j > 0; j-- {
+			if CompareTo(arr[j], arr[j-1]) == -1 {
+				Swap(&arr[j], &arr[j-1])
+			}
+		}
+	}
+	return arr
+}
+
+func Swap(i *int, j *int) {
+	*i, *j = *j, *i
+}
+
+func CompareTo(i int, j int) int {
+	if i > j {
+		return 1
+	} else if i < j {
+		return -1
+	} else {
+		return 0
+	}
+}
